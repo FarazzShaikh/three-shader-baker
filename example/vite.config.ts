fe0b7328ca4b6b19e94d74f0ce847b1ab8960cea
Faraz Shaikh <farazzshaikh@gmail.com>
2024-09-23 23:08:50 +0400

    Add vite base to example

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/three-shader-baker/",
   plugins: [react()],
   resolve: {
     alias: {
